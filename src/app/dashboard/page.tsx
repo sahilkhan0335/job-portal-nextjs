e2b6cdce5f58bb3fa862b75fbffadd8e63c10b41
sahilkhan0335 <sahil233582@gmail.com>
2025-11-04 18:47:28 +0530

    feat(auth): implement protected routes with role-based access control for Employer and Applicant dashboards

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ApplicantDashboard = () => {
+  return (
+    <>
+    <h1>Hbiefbxdbfjbj</h1>
+    </>
+
+  );
+}
+
+export default ApplicantDashboard;
---
+++ src/app/employer-dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployerDashboard = () => {
+  return (
+    <>
+    <h1>Welcome Employers</h1>
+    </>
+)
+}
+
+export default EmployerDashboard;
