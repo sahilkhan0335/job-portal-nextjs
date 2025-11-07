a7b3c06ae3eed7071822e48f4e07c8701a12984a
sahilkhan0335 <sahil233582@gmail.com>
2025-11-07 17:30:33 +0530

    feat(db): insert user data into Employer and Applicant tables based on role

--- src/features/auth/server/auth.actions.tsx
+++ src/features/auth/server/auth.actions.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { db } from "@/config/db";
-import { users } from "@/drizzle/schema";
+import { applicants, employers, users } from "@/drizzle/schema";
 import argon2 from "argon2";
 import { eq, or } from "drizzle-orm";
 import {
@@ -59,6 +59,11 @@ export const registerUserAction = async (data: RegisterUserData) => {
       .values({ name, userName, email, password: hashPassword, role });
 
     console.log(result);
+    if (role === "applicant") {
+      await db.insert(applicants).values({ id: result.insertId });
+    } else {
+      await db.insert(employers).values({ id: result.insertId });
+    }
 
     await createSessionAndSetCookies(result.insertId);
 
